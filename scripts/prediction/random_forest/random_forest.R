802a62b672ce5dfcf6497c0035f71d59942ead9b
Taylor Zowtuk <zowtukt@gmail.com>
2020-11-27 11:41:42 -0700

    Partition the data

--- scripts/prediction/random_forest/random_forest.R
+++ scripts/prediction/random_forest/random_forest.R
@@ -1,4 +1,5 @@
 library(readr)
+library(randomForest)
 source("random_forest_cleaning.R")
 
 # Set working dir to the path of this script; only works when using RStudio
@@ -9,3 +10,18 @@ baskets <- read_csv("random_forest.csv")
 baskets_with_multiple_cust <- read_csv("baskets_with_multiple_cust.csv")
 
 baskets <- clean_data_for_random_forest(baskets, baskets_with_multiple_cust)
+
+# Partition the training, eval, test sets using ratio 70:15:15
+set.seed(7)
+percent_train <- 7/10
+train_ids <- sample(1:nrow(baskets), size=percent_train*nrow(baskets) , replace=F)
+train_set <- baskets[train_ids, ]
+remaining <- baskets[-train_ids, ]
+percent_test <- 1/2
+test_ids <- sample(1:nrow(remaining), size=percent_test*nrow(remaining) , replace=F)
+valid_set <- baskets[-test_ids, ]
+test_set <- baskets[test_ids, ]
+
+# Train a random forest model
+
+
